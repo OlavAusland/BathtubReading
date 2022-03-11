900c4769d886081c7e26de80566614f8786650ed
Olav Ausland Onstad <olavao@uia.no>
2022-03-11 11:31:16 +0100

    Fixed Errors

--- App.js
+++ App.js
@@ -4,8 +4,9 @@ import { useEffect, useState } from "react";
 import { collection, doc, getDocs } from "firebase/firestore";
 
 import LoginPage from './components/Login.js'
-import HomePage from './components/Home.js'
+import ProfilePage from './components/Profile.js'
 import RegisterPage from './components/Register.js';
+import HomePage from './components/Home.js';
 
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
@@ -15,9 +16,10 @@ const Stack = createNativeStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator screenOptions={{headerShown: false}} initialRouteName='Login'>
-        <Stack.Screen name="Login" component={LoginPage}/>
+      <Stack.Navigator screenOptions={{headerShown: false}} initialRouteName='Home'>
         <Stack.Screen name="Home" component={HomePage}/>
+        <Stack.Screen name="Login" component={LoginPage}/>
+        <Stack.Screen name="Profile" component={ProfilePage}/>
         <Stack.Screen name="Register" component={RegisterPage}/>
       </Stack.Navigator>
     </NavigationContainer>
--- components/Home.js
+++ components/Home.js
@@ -1,130 +1,23 @@
-import { useEffect, useState} from 'react';
-import { View, Image, Text, Button, StyleSheet, ScrollView} from 'react-native';
-import { Card } from 'react-native-paper'
-import { db, storage } from "../firebase-config.js";
-import { getAuth, signOut, updateProfile } from 'firebase/auth';
-import { collection, doc, getDocs, setDoc, query, where } from "firebase/firestore";
-import { getDownloadURL, ref, uploadBytes} from 'firebase/storage';
-import { navigation, usenavigationParam} from '@react-navigation/native';
+import { useState, useEffect } from 'react';
+import { Text, View, Button, Image, StyleSheet} from 'react-native'
 
-export default function HomePage({ navigation })
+export default function HomePage()
 {
-    
-    const auth = getAuth();
-    
-    const [user, setUser] = useState(auth.currentUser);
-    const [avatar, setAvatar] = useState("");
-    const [library, setLibrary] = useState();
-    const [logout, setLogout] = useState(false);
-    const [image, setImage] = useState("");
-
-    useEffect(async () => {
-        await getDownloadURL(ref(storage, user.photoURL)).then((url) => setAvatar(url)).catch((error) => console.log(error));
-        console.log(user.photoURL)
-        const getLibrary = async() => {
-            const result = await getDocs(collection(db, "Books"));
-            const data = result.docs.map((doc) => ({...doc.data(), id: doc.id}));
-            setLibrary(data.json)
-        };
-
-        const getBook = async(isbn) => {
-            const result = await fetch(`https://www.googleapis.com/books/v1/volumes?q=isbn:${isbn}`);
-            const json = await result.json()
-            const link = json.items[0].volumeInfo.imageLinks.thumbnail
-            setImage(link);
-            console.log(link)
-        };
-
-        const setBook = async(isbn) => {
-            await setDoc(doc(db, 'Books', isbn), {
-                title: 'Python, Hero To Zero B-)'
-            });
-        }
-
-        getLibrary();
-        getBook('0735619670');
-        setBook('123');
-        console.log(library)
-    }, [user]);
-
-
-    useEffect(() => {
-        if(logout)
-        {
-            signOut(auth).then(() => {
-                console.log("Logged Out")
-                navigation.navigate('Login')
-            }).catch((error) => {
-                console.log(error);
-            })
-            setLogout(false);
-        }
-    }, [logout]);
-
-    if(user != null)
-    {
-        return (
-            <View style={{justifyContent:'center', alignItems:'center'}}>
-                <Image style={{borderRadius:250, width:250, height:250}}
-                    //'../assets/Images/Profile/Hoodie_V6.png'
-                    source={{uri: avatar}}
+    return (
+        <View style={[styles.container, {flexDirection:'row'}]}>
+            <View style={{ flex: 1, backgroundColor: "red" }}>
+                <Image
+                    source={require('../assets/Images/Loading.gif')}
                 />
-                <Text style={{fontSize:42, fontWeight:'bold'}}>Favorites</Text>
-                <ScrollView horizontal={true} style={styles.scroller} showsVerticalScrollIndicator={false}>
-                    <Image style={{width:'250px', height: '250px'}}
-                        source={{uri: image}}
-                    />
-                    <Image style={{width:'250px', height: '250px'}}
-                        source={{uri: image}}
-                    />
-                    <Image style={{width:'250px', height: '250px'}}
-                        source={{uri: image}}
-                    />
-                    <Image style={{width:'250px', height: '250px'}}
-                        source={{uri: image}}
-                    />
-                    <Image style={{width:'250px', height: '250px'}}
-                        source={{uri: image}}
-                    />
-                    <Image style={{width:'250px', height: '250px'}}
-                        source={{uri: image}}
-                    />
-                </ScrollView>
-                <Button title="Sign Out" onPress={() => setLogout(true)}/>
             </View>
-        );
-    }
-    else{
-        return (
-            <View style={{ justifyContent: 'center', alignItems: 'center', flex: 1}}>
-                <Text style={{fontSize:64}}>Loading...</Text>
-            </View>
-        );
-    }
+            <View style={{ flex: 1, backgroundColor: "darkorange" }} />
+        </View> 
+    );
 }
 
 const styles = StyleSheet.create({
     container:{
-        width:'100%',
-        height:'100%',
-        alignItems: 'center',
-        backgroundColor: 'linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(9,9,121,1) 35%, rgba(0,212,255,1)  %)'
-    },
-    card:{
-        width:'75%',
-        height:'20%',
-        shadowOffset: {width:0, height:6},
-        shadowColor: '#000000'
-    },
-    avatar:{
-        width:150,
-        height:150
-    },
-    scroller:{
-        width:'80%', 
-        height:'250px',
-        shadowColor: 'rgba(0, 0, 0, 0.3)',
-        shadowRadius: 6,
-        shadowOffset: {width:0, height:6}
+        justifyContent:'center',
+        flex:1
     }
 })
