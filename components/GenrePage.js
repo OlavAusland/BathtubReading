7e3209e9619150edcfd5c069e750a2e9a27b8711
Lin Julie Fjeldvik <linjf@uia.no>
2022-03-14 10:53:45 +0100

    Started on getting books
    Started on getting books
    
    Will continue with getting them by genre...

--- components/GenrePage.js
+++ components/GenrePage.js
@@ -1,23 +1,41 @@
 import { useState, useEffect } from 'react';
 import { Text, View, Button, Image, StyleSheet} from 'react-native'
+import { getBook } from '../API/GoogleAPI.js';
+import { getFirebaseBook, getFirebaseBooks } from '../API/FirebaseAPI.js';
 
-export default function GenrePage(){
+const GenrePage = props => {
 
+    const [books, setbooks] = useState('');
+
+    useEffect(() => {
+        const getBook = async (isbn) => {
+            const data = await getBook('9780132856201');
+            const firebaseData = await getFirebaseBook('9780132856201');
+            const firebaseDatas = await getFirebaseBooks();
+            setbooks({
+                title: data.items[0].volumeInfo.title,
+                imageURI: data.items[0].volumeInfo.imageLinks.thumbnail
+            }); 
+        }
+        console.log(books)
+        getBook();
+    }, []);
+    
     return(
-        <View style={styles.container}>
-            <Text>
-                HELLO
-            </Text>
+        <View style={styles.Bookscontainer}>
+            HELLO
         </View>
-    );
 
+    );
 }
 
 const styles = StyleSheet.create({
-    container:{
+    Bookscontainer:{
         flex: 1,
         backgroundColor: '#fff',
         alignItems: 'center',
         justifyContent: 'center',
     },
 })
+
+export default GenrePage
