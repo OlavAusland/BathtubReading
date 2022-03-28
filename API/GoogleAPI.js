fd1dc4000b9acfa65e57fb34ac2fd651344a7616
Olav Ausland Onstad <olavao@uia.no>
2022-03-28 09:49:54 +0200

    Extracted Profile Files

--- API/GoogleAPI.js
+++ API/GoogleAPI.js
@@ -5,11 +5,6 @@ export async function getBook(isbn){
     const result = await fetch(`https://www.googleapis.com/books/v1/volumes?q=isbn:${isbn}`);
     const json = await result.json()
     return json
-    /*
-    const link = json.items[0].volumeInfo.imageLinks.thumbnail
-    setImage(link);
-    console.log(link)
-    */
 };
 
 
--- components/Profile.jsx
+++ components/Profile.jsx
@@ -5,89 +5,28 @@ import { getAuth, signOut, updatePassword } from 'firebase/auth';
 import { collection, getDocs } from "firebase/firestore";
 import { getDownloadURL, ref} from 'firebase/storage';
 //import { getBook } from '../api/googleAPI'
-import { getBooks, getBook, getUserLibrary, updateUser } from '../API/FirebaseAPI'
+import { getBooks, updateUser } from '../api/firebaseAPI'
 import { profileStyle } from '../styles/ProfileStyles' 
 import BookPage from './Book.jsx';
+import { DisplayUserLists } from './profile/DisplayUserLists.jsx';
+import { GetUserListsInformation } from './profile/GetUserListsInformation.jsx';
+import { ProfileModal } from './profile/ProfileModal.jsx';
 
-function DisplayUserLists(library)
-{
-    return [...Array.from(library.keys())].map((key) => {
-        if(key == 'favorites'){return;}
-        return(
-            <View key={ Math.random().toString(36).substr(2, 9)}>
-                <Text style={{fontWeight:'bold', fontSize:30}}>{key.toUpperCase()}</Text>
-                <ScrollView horizontal={true}>
-                    {library.get(key).map((book, index) => {
-                        return(
-                            <View key={`${key}-${index}`}>
-                                <TouchableOpacity onPress={() => navigation.navigate('Book')}>
-                                    <Image
-                                        style={profileStyle.image}
-                                        source={{uri:book.imageURI}}
-                                    />
-                                </TouchableOpacity>
-                                <Text style={{overflow:'hidden'}}>{book.title}</Text>
-                            </View>
-                            
-                        );
-                    })}
-                </ScrollView>
-            </View>
-        )
-    })
-}
-
-async function GetUserListsInformation(user)
-{
-    let userLibrary = [];
-    let isbnArray = new Map();
-    let keys = []
-    const lists = await getUserLibrary(user.uid);
-    lists.map((list) => {
-        return Object.keys(list).map((key) => {
-            let temp = [];
-            [...Array(list[key].length).keys()].map((i) => {
-                temp.push(getBook(list[key][i]))
-            })
-            keys.push(key)
-            isbnArray.set(key, temp)
-        })
-    });
-    
-    const resolve = async() => {
-        const test = new Map()
-        for(let i = 0; i < keys.length;i++)
-        {
-            await Promise.all(isbnArray.get(keys[i])).then((res) => {test.set(keys[i], res);});
-        }
-        return test;
-    }
-
-
-    return resolve();
-}   
 
 export default function ProfilePage({ navigation })
 {
     const auth = getAuth();
-    const [checked, setChecked] = useState(false);
     const [modalVisible, setModalVisible] = useState(false);
     const [user, setUser] = useState(auth.currentUser);
     const [avatar, setAvatar] = useState("");
     const [library, setLibrary] = useState(new Map());
     const [logout, setLogout] = useState(false);
-    const [image, setImage] = useState("");
     const [loading, setLoading] = useState(false);
-
-    const [username, setUsername] = useState(user ? user.displayName : "");
-    const [password, setPassword] = useState("");
-    const [retypedPassword, setRetypedPassword] = useState("");
-    const [passwordError, setPasswordError] = useState("");
     
     useEffect(async() => {
         await getDownloadURL(ref(storage, user.photoURL)).then((url) => setAvatar(url)).catch((error) => console.log(error));
-        const test = await GetUserListsInformation(user);
-        setLibrary(test)
+        const userBooks = await GetUserListsInformation(user);
+        setLibrary(userBooks)
     }, [user]);
 
     useEffect(async() => {
@@ -96,8 +35,6 @@ export default function ProfilePage({ navigation })
             return firebaseData;
           }
           const books = await getMybooks();
-          //setLibrary(books);
-          //setBook('9783319195957')
 
     }, []); 
 
@@ -105,7 +42,6 @@ export default function ProfilePage({ navigation })
         const getLibrary = async() => {
             const result = await getDocs(collection(db, "Books"));
             const data = result.docs.map((doc) => ({...doc.data(), id: doc.id}));
-            //setLibrary(data)
         };
         getLibrary();
     }, [])
@@ -127,98 +63,7 @@ export default function ProfilePage({ navigation })
     {
         return (
             <View style={[profileStyle.container, {flexDirection:'column'}]}>
-                <Modal
-                    animationType="slide"
-                    statusBarTranslucent={true}
-                    transparent={true}
-                    visible={modalVisible}
-                    onRequestClose={() => {
-                    Alert.alert("Modal has been closed.");
-                    setModalVisible(!modalVisible);
-                    }}
-                >
-                    <View style={profileStyle.modal}>
-                        <View style={profileStyle.modalView}>
-                            <View style={{flex:6, paddingTop:10}}>
-                                <View>
-                                    <View style={{flexDirection:'row', minWidth:'100%'}}>
-                                        <Text style={{flex:1}}>Username</Text>
-                                        <View style={{flex:1}}/>
-                                    </View>
-                                    <View style={{flexDirection:'row', minWidth:'100%'}}>
-                                        <TextInput
-                                            onChangeText={updated => setUsername(updated)}
-                                            style={profileStyle.input}>{user.displayName}
-                                        </TextInput>
-                                        <Pressable style={[profileStyle.checkAvailabilityButton, {flex:1}]}
-                                        
-                                        ><Text>Check Availabilty</Text></Pressable>
-                                    </View>
-                                </View>
-                                <View>
-                                    <View style={{flexDirection:'row', minWidth:'100%'}}>
-                                        <Text style={{flex:1}}>Email:</Text>
-                                    </View>
-                                    <View style={{flexDirection:'row', minWidth:'100%'}}>
-                                        <TextInput
-                                            editable={false}
-                                            style={[profileStyle.input, {backgroundColor:'lightgrey'}]}>{user.email}
-                                        </TextInput>
-                                    </View>
-                                </View>
-                                <View>
-                                    <View style={{flexDirection:'row', minWidth:'100%'}}>
-                                        <Text style={{flex:1}}>Password:</Text>
-                                    </View>
-                                    <View style={{flexDirection:'row', minWidth:'100%'}}>
-                                        <TextInput onChangeText={updated => setPassword(updated)} 
-                                        secureTextEntry={true} style={profileStyle.input}/>
-                                    </View>
-                                </View>
-                                <View>
-                                    <View style={{flexDirection:'row', minWidth:'100%'}}>
-                                        <Text style={{flex:1}}>Retype Password:</Text>
-                                    </View>
-                                    <View style={{flexDirection:'row', minWidth:'100%'}}>
-                                        <TextInput 
-                                            onChangeText={updated => setRetypedPassword(updated)} 
-                                            secureTextEntry={true} style={profileStyle.input}/>
-                                    </View>
-                                </View>
-                                <View>
-                                    <View style={{flexDirection:'row', minWidth:'100%'}}>
-                                        {password != retypedPassword &&
-                                            <Text style={{color:'red', fontWeight:'bold'}}>Password Does Not Match!</Text>
-                                        }
-                                        {passwordError != "" &&
-                                            <Text style={{color:'red', fontWeight:'bold'}}>{passwordError}</Text>
-                                        }
-                                    </View>
-                                </View>
-                            </View>
-                            <View style={{flex:6}}/>
-                            <View style={{flex:2, flexDirection:'row', width:'100%', backgroundColor:'#F6EEE0'}}>
-                                <Pressable
-                                        style={profileStyle.modalButton}
-                                        onPress={async() => {updateUser(username, password);
-                                            if(password == retypedPassword && password != "")
-                                            {
-                                                await updatePassword(auth.currentUser, password).then(setPasswordError("")).catch((error) => 
-                                                {console.log(error);setPasswordError(error.errorMessage)});
-                                            }setModalVisible(false);}}
-                                        >
-                                    <Text style={{fontSize:18, fontWeight:'bold'}}>Update</Text>
-                                </Pressable>
-                                <Pressable
-                                        style={profileStyle.modalButton}
-                                        onPress={() => {setModalVisible(false); }}
-                                    >
-                                    <Text style={{fontSize:18, fontWeight:'bold'}}>Close</Text>
-                                </Pressable>
-                            </View>
-                        </View>
-                    </View>
-                </Modal>
+                <ProfileModal modalVisible={modalVisible} user={user} setModalVisible={setModalVisible}/>
                 <View style={profileStyle.header}>
                     <View style={{flex:10, justifyContent:'center', alignItems:'center'}}>
                         <Image style={profileStyle.avatar}
@@ -230,13 +75,11 @@ export default function ProfilePage({ navigation })
                         <Text style={{fontSize:18}}>{user.email}</Text>
                     </View>
                 </View>
-                {/*
                  <View style={profileStyle.settings}>
                     <Pressable onPress={() => setModalVisible(true)} style={profileStyle.settingsButton}>
                         <Text style={{flex:1, fontSize:20, fontWeight:'bold', justifyContent:'center', alignItems:'center'}}>Settings</Text>
                     </Pressable>
                 </View>
-                */}
                 <View style={profileStyle.content}>
                     <View style={{flex:3, width:'90%'}}>
                         <Text style={{fontWeight:'bold', fontSize:30}}>FAVORITES</Text>
