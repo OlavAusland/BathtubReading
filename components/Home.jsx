c0ea38b7ed16cec225a78ca4dfaf6cacd5cbc833
Jeanett Wesstøl <jeanettwe@uia.no>
2022-03-29 16:19:45 +0200

    fix search

--- components/Home.jsx
+++ components/Home.jsx
@@ -2,15 +2,14 @@ import React, { useState, useEffect } from 'react';
 import { Text, View, Button, Image, StyleSheet, TextInput, ScrollView, TouchableOpacity } from 'react-native';
 import * as firebaseApi from "../api/firebaseAPI.js";
 import { SearchResultsView } from "./home/SearchResultsView";
-import { GenreView} from "./home/GenreView"
+import { GenreView } from "./home/GenreView"
 import { DefaultHome } from './home/DefaultView';
 import { homeStyles } from '../styles/HomeStyles';
 import { getAllGenres } from '../api/firebaseAPI.js';
 import Icon from 'react-native-vector-icons/FontAwesome';
 
-
 export default function HomePage({ navigation }) {
-    
+
     const [allGenres, setAllGenres] = useState([]);
     const [searchKeyword, setSearchKeyword] = useState('');
     const [queryGenre, setQueryGenre] = useState('');
@@ -20,8 +19,8 @@ export default function HomePage({ navigation }) {
 
     const handleGenreChange = (val) => {
         setSearching(false)
-        if(queryGenre === val){setDisplayGenre(false);setQueryGenre('')}
-        else{setDisplayGenre(true);setQueryGenre(val)}
+        if (queryGenre === val) { setDisplayGenre(false); setQueryGenre('') }
+        else { setDisplayGenre(true); setQueryGenre(val) }
     }
 
     const handleSearch = (event) => {
@@ -29,31 +28,32 @@ export default function HomePage({ navigation }) {
         setSearching(searchKeyword.length > 0);
     }
 
-    useEffect(async() => {const genres = await getAllGenres(); setAllGenres(genres)}, []);
+    useEffect(async () => { const genres = await getAllGenres(); setAllGenres(genres) }, []);
 
     return (
         <View style={[homeStyles.container, { flexDirection: 'column' }]}>
-            <View style={{ flex: 3, backgroundColor: "#194a50", borderBottomColor: 'black'}}>
-                <View><Text style={{ fontSize: 50, marginLeft: 10, color: 'white'}}> Discovery </Text></View>
-                <View style={{width:'55%',borderRadius:10, marginLeft:25, marginTop:10, backgroundColor:'#FFFFFF', flexDirection: 'row'}}>
-                    <View style={{height:'80%', marginRight:10, marginLeft:5, marginTop:2}}>
+            <View style={{ flex: 3, backgroundColor: "#194a50", borderBottomColor: 'black' }}>
+                <View><Text style={{ fontSize: 50, marginLeft: 10, color: 'white' }}> Discovery </Text></View>
+                <View style={{ width: '55%', borderRadius: 10, marginLeft: 25, marginTop: 10, backgroundColor: '#FFFFFF', flexDirection: 'row' }}>
+                    <View style={{ height: '80%', marginRight: 10, marginLeft: 5 }}>
                         <Icon name="search" size={20} color="#000000" />
                     </View>
                     <TextInput
                         onEndEditing={(e) => handleSearch(e)}
-                        onPressIn={() => {setSearching(true)}}
+                        onPressIn={() => { setSearching(true) }}
                         placeholder="Search"
+                        style={{ width: '90%'}}
                     />
                 </View>
             </View>
-            <View style={{ flex:1, backgroundColor: "#FFFFFF", borderBottomColor: 'black'}}>
+            <View style={{ flex: 1, backgroundColor: "#FFFFFF", borderBottomColor: 'black' }}>
                 <ScrollView style={homeStyles.scroller} horizontal={true} showsHorizontalScrollIndicator={false}>
                     {allGenres.length > 0 &&
                         allGenres.map((elem, index) => {
                             return (
                                 <View key={elem} style={{ alignItems: 'center', justifyContent: 'center' }}>
                                     <View style={homeStyles.scrollButtons}>
-                                        <Button title={elem.toUpperCase()} color="#194a50" onPress={() => {handleGenreChange(elem);}} />
+                                        <Button title={elem.toUpperCase()} color="#194a50" onPress={() => { handleGenreChange(elem); }} />
                                     </View>
                                 </View>
                             )
@@ -62,9 +62,9 @@ export default function HomePage({ navigation }) {
                 </ScrollView>
             </View>
             <View style={{ flex: 9, backgroundColor: "#194a50" }}>
-                {(!displayGenre && !searching ) && <DefaultHome navigation={navigation}/>}
-                {(displayGenre && !searching ) && <GenreView genre={queryGenre} navigation={navigation}/>}
-                {(searching) && <SearchResultsView books={books} navigation={navigation} keyword={searchKeyword}/>} 
+                {(!displayGenre && !searching) && <DefaultHome navigation={navigation} />}
+                {(displayGenre && !searching) && <GenreView genre={queryGenre} navigation={navigation} />}
+                {(searching) && <SearchResultsView books={books} navigation={navigation} keyword={searchKeyword} />}
             </View>
         </View>
     );
--- components/Login.jsx
+++ components/Login.jsx
@@ -7,7 +7,7 @@ import Icon from 'react-native-vector-icons/FontAwesome';
 
 
 export default function LoginPage({ navigation }) {
-    
+
     const auth = getAuth();
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
@@ -32,23 +32,22 @@ export default function LoginPage({ navigation }) {
 
     return (
         <View style={{ width: '100%', justifyContent: 'center', alignItems: 'center', flex: 1, backgroundColor: '#194a50' }}>
-             <View style={{marginBottom:60}}>
-                 <Icon name="user-circle-o" size={200} color="#FFFFFF" />
-                </View>
-            <View style={{ width: '100%', justifyContent: 'center', alignItems: 'center', flexDirection:'row'}}>
-                <View style={{height:'85%', marginRight:10}}>
-                 <Icon name="envelope" size={30} color="#FFFFFF" />
+            <View style={{ marginBottom: 60 }}>
+                <Icon name="user-circle-o" size={200} color="#FFFFFF" />
+            </View>
+            <View style={{ width: '100%', justifyContent: 'center', alignItems: 'center', flexDirection: 'row' }}>
+                <View style={{ height: '85%', marginRight: 10 }}>
+                    <Icon name="envelope" size={30} color="#FFFFFF" />
                 </View>
                 <TextInput
                     style={loginStyle.input}
                     placeholder="Email"
                     onChangeText={updated => setEmail(updated)}
                 />
-
             </View>
-            <View style={{ width: '100%', justifyContent: 'center', alignItems: 'center', flexDirection:'row' }}>
-            <View style={{height:'85%', marginRight:10}}>
-                 <Icon name="key" size={30} color="#FFFFFF" />
+            <View style={{ width: '100%', justifyContent: 'center', alignItems: 'center', flexDirection: 'row' }}>
+                <View style={{ height: '85%', marginRight: 10 }}>
+                    <Icon name="key" size={30} color="#FFFFFF" />
                 </View>
                 <TextInput
                     style={loginStyle.input}
@@ -56,28 +55,28 @@ export default function LoginPage({ navigation }) {
                     placeholder="Password"
                     onChangeText={updated => setPassword(updated)} />
             </View>
-            
-                {error && <Text style={{ color: 'rgb(255, 0, 0)', fontSize: 18, alignSelf: 'center' }}>{error}</Text>}
-                <View style={{ width: '100%', justifyContent: 'center', alignItems: 'center', flexDirection:'row'}}>
-                <View style={{height:'60%', marginRight:10}}>
-                        <Icon name="user-o" size={30} color="#FFFFFF" />
-                    </View>
-                    <Pressable
-                        style={loginStyle.loginButtons}
-                        onPress={() => setLogin(true)}>
-                        <Text style={loginStyle.buttontext}>Login</Text>
-                    </Pressable>
+
+            {error && <Text style={{ color: 'rgb(255, 0, 0)', fontSize: 18, alignSelf: 'center' }}>{error}</Text>}
+            <View style={{ width: '100%', justifyContent: 'center', alignItems: 'center', flexDirection: 'row' }}>
+                <View style={{ height: '60%', marginRight: 10 }}>
+                    <Icon name="user-o" size={30} color="#FFFFFF" />
                 </View>
-                <View style={{ width:'100%', justifyContent: 'center', alignItems: 'center', flexDirection:'row'}}>
-                    <View style={{height:'60%', marginRight:3}}>
-                        <Icon name="user-plus" size={30} color="#FFFFFF" />
-                    </View>
-                    <Pressable
-                        style={loginStyle.loginButtons}
-                        onPress={() => navigation.navigate("Register")}>
-                        <Text style={loginStyle.buttontext}> Register</Text>                
-                    </Pressable>
+                <Pressable
+                    style={loginStyle.loginButtons}
+                    onPress={() => setLogin(true)}>
+                    <Text style={loginStyle.buttontext}>Login</Text>
+                </Pressable>
+            </View>
+            <View style={{ width: '100%', justifyContent: 'center', alignItems: 'center', flexDirection: 'row' }}>
+                <View style={{ height: '60%', marginRight: 3 }}>
+                    <Icon name="user-plus" size={30} color="#FFFFFF" />
                 </View>
+                <Pressable
+                    style={loginStyle.loginButtons}
+                    onPress={() => navigation.navigate("Register")}>
+                    <Text style={loginStyle.buttontext}> Register</Text>
+                </Pressable>
+            </View>
         </View>
     );
 }
