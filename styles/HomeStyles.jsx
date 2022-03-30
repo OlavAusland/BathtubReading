51d29ac44434475b77a3237eccd19d88c0ee987a
Olav Ausland Onstad <olavao@uia.no>
2022-03-30 09:43:24 +0200

    Extracted Styles

--- styles/HomeStyles.jsx
+++ styles/HomeStyles.jsx
@@ -31,3 +31,50 @@ export const homeStyles = StyleSheet.create({
         backgroundColor: 'black'
     }
 })
+
+export const defaultViewStyle = StyleSheet.create({
+    Container:{
+        padding: 24,
+        backgroundColor: "#F6EEE0"
+    },
+    Title:{
+        alignSelf:"center",
+        fontSize:45,
+    },
+    Booklisting:{
+        alignContent: "flex-start",
+    },
+    BookContainer:{
+        flex: 1,
+        padding:24,
+        backgroundColor: "#E4B7A0",
+        alignItems: "baseline"
+    },
+    BookCard:{
+        backgroundColor: "#194a50",
+        borderRadius: 8,
+        paddingVertical: 45,
+        paddingHorizontal:25,
+        width: "75%",
+        marginVertical: 10,
+        alignItems: 'center'
+    },
+    Booktitle:{
+        fontSize: 15,
+        fontWeight: 'normal',
+        color:'white',
+        alignContent: "center"
+    },
+    Imagestyle:{
+        width: 200,
+        height: 200,
+        borderRadius:10
+
+    },
+    shadowProp:{
+        shadowColor: '#100f0f',
+        shadowOffset: {width: -2, height: 4},
+        shadowOpacity: 0.5,
+        shadowRadius: 5
+    }
+})
